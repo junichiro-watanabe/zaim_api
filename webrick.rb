d2e5f227d4bc57f7942b943eeb6d8f0120667721
Your Name <junichiro_watanabe@watanavi.com>
2020-11-22 05:13:33 +0900

    全体の流れを記載

--- webrick.rb
+++ webrick.rb
@@ -1,11 +1,11 @@
 require 'webrick'
 
 server = WEBrick::HTTPServer.new({
-  DocumentRoot:   './',
-  BindAddress:    '0.0.0.0',
-  CGIInterpreter: '/usr/local/bin/ruby',
-  Port:           3000
-})
+                                   DocumentRoot: './',
+                                   BindAddress: '0.0.0.0',
+                                   CGIInterpreter: '/usr/local/bin/ruby',
+                                   Port: 3000
+                                 })
 
 server.mount('/', WEBrick::HTTPServlet::CGIHandler, 'sample_code.rb')
 
