159ddadcc671a07ce4f628a3a11677ef96940d64
Your Name <junichiro.watanabe.1113@gmail.com>
2020-11-24 02:10:02 +0900

    セッションリセット機能を追加

--- sample_code.rb
+++ sample_code.rb
@@ -4,19 +4,22 @@ require 'cgi'
 require 'cgi/session'
 require 'json'
 
-##### ユーザ固有情報を格納してください ###################################################
-consumer_key = "0ee0e839a43ec6c737070875125ca168da422175"     # コンシューマ ID
-consumer_secret = "b6e4eab08dc3e923cfc0eea342e56415c34c3814"  # コンシューマシークレット
-callback_url = 'http://localhost:3000'                        # コールバック先URL
-########################################################################################
+##### ユーザ固有情報を格納してください ###########################################################
+# zaim へのアプリケーション登録後に発行されるコンシューマ ID
+consumer_key = "<input your consumer_key>"
+# zaim へのアプリケーション登録後に発行されるコンシューマシークレット ID
+consumer_secret = "<input your consumer_secret>"
+# zaim での認証後のコールバック先 URL
+callback_url = "<input your callback_url>"
+###############################################################################################
 
 # Zaim API 情報格納する
-site = 'https://api.zaim.net' # ルートパス
-provider_base = 'https://api.zaim.net/v2/auth/'           # OAuth ベースパス
-request_url = "#{provider_base}request"                   # request token リクエストパス
-authorize_url = "https://auth.zaim.net/users/auth"        # 認証 パス
-access_url = "#{provider_base}access"                     # access token リクエストパス
-resource_url = 'https://api.zaim.net/v2/home/user/verify' # リソースパス
+site = 'https://api.zaim.net'                             # ルート URL
+provider_base = 'https://api.zaim.net/v2/auth/'           # OAuth ベース URL
+request_url = "#{provider_base}request"                   # request token リクエスト URL
+authorize_url = "https://auth.zaim.net/users/auth"        # 認証 URL
+access_url = "#{provider_base}access"                     # access token リクエスト URL
+resource_url = 'https://api.zaim.net/v2/home/user/verify' # アクセス先 URL
 
 # OAuth Consumer インスタンスを生成
 oauth_consumer = OAuth::Consumer.new(consumer_key, consumer_secret,
@@ -25,28 +28,35 @@ oauth_consumer = OAuth::Consumer.new(consumer_key, consumer_secret,
                                      authorize_url: authorize_url,
                                      access_token_url: access_url)
 
+# CGIインスタンス生成
+cgi = CGI.new
+session = CGI::Session.new(cgi)
+
+# セッションクリア処理
+if !cgi.params['action'].empty? && \
+   cgi.params['action'][0] == 'clear'
+
+  session = CGI::Session.new(cgi, { "new_session" => true })
+end
+
 # 結果表示部分
 @content = ''
 
 # 処理開始
 begin
-  # CGIインスタンス生成
-  cgi = CGI.new
-  session = CGI::Session.new(cgi)
-
-  # ２．認証処理
+  # ２．認証処理(初回は session['type'] = nil のため実行されない)
   if session['type'] == 'authorize' && \
-     !session['oauth_token'].nil? && \
-     !session['oauth_token_secret'].nil?
+     !cgi.params['oauth_token'][0].nil? && \
+     !cgi.params['oauth_verifier'][0].nil?
 
     request_token = OAuth::RequestToken.new(oauth_consumer, session['oauth_token'], session['oauth_token_secret'])
-    access_token = request_token.get_access_token(oauth_verifier: cgi.params[:oauth_verifier])
+    access_token = request_token.get_access_token(oauth_verifier: cgi.params['oauth_verifier'][0])
     session['type'] = 'access'
     session['oauth_token'] = access_token.token
     session['oauth_token_secret'] = access_token.secret
   end
 
-  # ３．アクセス処理
+  # ３．アクセス処理(初回は session['type'] = nil のため実行されない)
   if session['type'] == 'access'
     access_token = OAuth::AccessToken.new(oauth_consumer, session['oauth_token'], session['oauth_token_secret'])
     response = access_token.get(resource_url)
